4875706d9783894d378ff03625d97661292cf2aa
Erick Valencia <evalencia0911@gmail.com>
2024-03-23 14:51:44 -0600

    fix people_collection.models

--- people_collection/models.py
+++ people_collection/models.py
@@ -12,14 +12,3 @@ class People(models.Model):
     
 
 # Create your models here.
-
-class People_Limbs(models.Model):
-    person = models.OneToOneField(
-        People,
-        on_delete=models.CASCADE,
-        primary_key=True,
-    )
-    arm=models.IntegerField()
-    leg=models.IntegerField()
-    feet=models.IntegerField()
-    hands=models.IntegerField()
